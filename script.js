539ccf373056f2f6822d4487819a407c03f554a0
David-Shibley <davidjshibley@gmail.com>
2015-11-03 20:32:49 -0700

    added random colors

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 // Your JS goes here
 var divs = [];
 
-var mapBoard = function(n) {
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function mapBoard(n) {
   for (var i = 0; i < n; i++) {
     console.log(i);
     divs[i] = document.createElement('div');
     divs[i].style.float = "left";
     divs[i].style.paddingBottom = "11.1%";
     divs[i].style.width = "11.1%";
+    divs[i].style.backgroundColor = getRandomColor();
     document.body.appendChild(divs[i])
     // if (i== n-1) {
     //   document.body.appendChild(divs[i]);
@@ -17,11 +27,11 @@ var mapBoard = function(n) {
     // } else {
     //   divs[i].appendChild(divs[i-1]);
     //}
-    if (i % 2 == 0) {
-      divs[i].style.backgroundColor = "black";
-    } else {
-      divs[i].style.backgroundColor = "red";
-    }
+    // if (i % 2 == 0) {
+    //   divs[i].style.backgroundColor = "black";
+    // } else {
+    //   divs[i].style.backgroundColor = "red";
+    // }
   };
 }
 
