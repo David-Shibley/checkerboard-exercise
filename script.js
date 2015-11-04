bbb38859c133801dbac5ec317c27fd14df1a189e
David-Shibley <davidjshibley@gmail.com>
2015-11-03 20:03:29 -0700

    checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 // Your JS goes here
+var divs = [];
+
+var mapBoard = function(n) {
+  for (var i = 0; i < n; i++) {
+    console.log(i);
+    divs[i] = document.createElement('div');
+    divs[i].style.float = "left";
+    divs[i].style.paddingBottom = "11.1%";
+    divs[i].style.width = "11.1%";
+    document.body.appendChild(divs[i])
+    // if (i== n-1) {
+    //   document.body.appendChild(divs[i]);
+    //   divs[i].appendChild(divs[i-1])
+    // } else if (i == 0) {
+    //
+    // } else {
+    //   divs[i].appendChild(divs[i-1]);
+    //}
+    if (i % 2 == 0) {
+      divs[i].style.backgroundColor = "black";
+    } else {
+      divs[i].style.backgroundColor = "red";
+    }
+  };
+}
+
+mapBoard(81);
